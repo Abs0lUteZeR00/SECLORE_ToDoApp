6d5a9e172a12aa7616c3e3e57cc369b5d7a5ad2b
Manohar1822 <86408578+Manohar1822@users.noreply.github.com>
2024-03-11 14:39:10 +0530

    Add files via upload

---
+++ SECLORE_ToDoApp/src/main/java/com/todo/main/SecloreToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.todo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecloreToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecloreToDoAppApplication.class, args);
+	}
+
+}
