107aaf6d002436f9d7c6992ffaba36575293495c
Anurag <anurag1000chauhan@gmail.com>
2024-03-12 13:11:28 +0530

    Interceptor

--- SECLORE_ToDoApp/src/main/java/com/todo/main/interceptor/ValidationInterceptor.java
+++ SECLORE_ToDoApp/src/main/java/com/todo/main/interceptor/ValidationInterceptor.java
@@ -4,14 +4,28 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
-public class ValidationInterceptor implements HandlerInterceptor{
+public class ValidationInterceptor implements HandlerInterceptor {
 	public ValidationInterceptor() {
 		// TODO Auto-generated constructor stub
 	}
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		HttpSession httpSession = request.getSession();
+		String uri = request.getRequestURI();
+
+		if (uri.equals("/") || uri.equals("/login") || uri.equals("/authenticate") || uri.equals("/register") || uri.equals("/signup")) {
+			return true;
+		}
+
+		if (httpSession.getAttribute("user") == null) {
+			httpSession.setAttribute("message", "Please Login First");
+			response.sendRedirect("/");
+			return false;
+		}
 		return true;
 	}
 }
